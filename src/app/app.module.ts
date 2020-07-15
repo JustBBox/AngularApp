d80b4b3279e4e4edc296ae40eb09d209b8424d09
EightBitBox <OsipovIvanS@yandex.ru>
2020-07-15 06:54:09 +0300

    Basics Final. Added UI Template and Mocks for Models.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,29 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { ShoppingListComponent } from './shopping-list/shopping-list.component';
+import { RecipeListComponent } from './recipes/recipe-list/recipe-list.component';
+import { HeaderComponent } from './header/header.component';
+import { RecipeDetailComponent } from "./recipes/recipe-detail/recipe-detail.component";
+import { RecipeItemComponent } from "./recipes/recipe-list/recipe-item/recipe-item.component";
+import { ShoppingListEditComponent } from "./shopping-list/shopping-list-edit/shopping-list-edit.component";
+import { RecipesComponent } from './recipes/recipes.component';
+import {FormsModule} from "@angular/forms";
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ShoppingListComponent,
+    RecipeListComponent,
+    HeaderComponent,
+    RecipeDetailComponent,
+    RecipeItemComponent,
+    ShoppingListEditComponent,
+    RecipesComponent
   ],
   imports: [
     BrowserModule,
+    FormsModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/shopping-list/shopping-list-edit/shopping-list-edit.component.ts
@@ -0,0 +1,9 @@
+import {Component} from "@angular/core";
+
+@Component({
+  selector: 'app-shopping-edit',
+  templateUrl: './shopping-list-edit.component.html'
+})
+export class ShoppingListEditComponent {
+
+}
